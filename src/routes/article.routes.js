26fde097900c8425af389d72331f6740c9910f3a
Jagadeesh <jagadeeshthegeek@gmail.com>
2021-01-13 06:00:48 +0530

    cu

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,26 +1,26 @@
-const articles = require("../controllers/article.controller.js");
+const articleCtrl = require("../controllers/article.controller.js");
 
 var router = require("express").Router();
 
 // Create a new Article
-router.post("/", articles.create);
+router.post("/", articleCtrl.create);
 
 // Retrieve all Articles
-router.get("/", articles.findAll);
+router.get("/", articleCtrl.findAll);
 
 // Retrieve all published Articles
-router.get("/published", articles.findAllPublished);
+router.get("/published", articleCtrl.findAllPublished);
 
 // Retrieve a single Article with id
-router.get("/:id", articles.findOne);
+router.get("/:id", articleCtrl.findOne);
 
 // Update a Article with id
-router.put("/:id", articles.update);
+router.put("/:id", articleCtrl.update);
 
 // Delete a Article with id
-router.delete("/:id", articles.delete);
+router.delete("/:id", articleCtrl.delete);
 
 // Create a new Article
-router.delete("/", articles.deleteAll);
+router.delete("/", articleCtrl.deleteAll);
 
 module.exports = router;
